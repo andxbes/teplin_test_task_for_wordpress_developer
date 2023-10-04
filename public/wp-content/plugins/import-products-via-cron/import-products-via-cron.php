f789e0e81cfe34b293b2a0e4c0d0b4c38873508c
Andrey Bescenniy <andxbes@gmail.com>
2023-10-04 12:04:29 +0300

    install plugin

---
+++ public/wp-content/plugins/import-products-via-cron/import-products-via-cron.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Import products via cron
+Plugin URI: https://akismet.com/
+Description: 
+Version: 1 
+Requires PHP: 8.0.0
+Author: 
+Author URI: 
+License: GPLv2 or later
+Text Domain: 
+*/
+class ImportProductsViaCron
+{
+    public function __construct()
+    {
+    }
+}
+
+new ImportProductsViaCron();
